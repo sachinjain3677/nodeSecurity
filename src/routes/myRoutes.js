2e17b82d134fdbbce50c03b9f60ec9f8a556ca79
Sachin Jain <sachinjain3677@gmail.com>
2020-04-21 13:58:37 +0530

    add /contacts route

--- src/routes/myRoutes.js
+++ src/routes/myRoutes.js
@@ -1,8 +1,30 @@
+import {getAllContacts,
+        addNewContact,
+        deleteContact,
+        getContactByID,
+        updateContact} from './../controllers/myControllers';
+
 const routes = (app) => {
   app.route('/')
     .get((req, res) => {
       console.log(`Get request on path /`);
-    })
+    });
+
+  app.route('/contacts')
+    .get((req, res, next) => {
+      console.log(`Get request on /contacts`);
+      next();
+    }, getAllContacts)
+
+    .post((req, res, next) => {
+      console.log(`POST request on /contacts`);
+      next();
+    }, addNewContact)
+
+    .delete((req, res, next) => {
+      console.log(`DELETE request on /contacts`);
+      next();
+    }, deleteContact);
 }
 
 export default routes;
