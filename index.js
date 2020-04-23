f5c6107580298358cda94434347316c4f457adac
Sachin Jain <sachinjain3677@gmail.com>
2020-04-23 13:29:36 +0530

    add mongo connection

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import process from 'process';
 import routes from './src/routes/myRoutes';
+import mongoose from 'mongoose';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/prac_db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
 
 routes(app);
 
