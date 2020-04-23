bd398e8743635fb504c1676c802bcd920be2b876
Sachin Jain <sachinjain3677@gmail.com>
2020-04-23 16:42:46 +0530

    add rejection handle, body-parser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import process from 'process';
 import routes from './src/routes/myRoutes';
 import mongoose from 'mongoose';
-
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,7 +11,13 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/prac_db', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
+}, (err) => {
+  if(err)
+    console.log(`Connection error`);
 });
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
 routes(app);
